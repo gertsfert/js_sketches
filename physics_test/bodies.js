541865d4a7c99352c0e79b79cb2a4c72ac2d0667
Alex Coleman <gertsfert@orthros.gateway>
2018-08-21 18:18:06 +1000

    add obstacle (ground) and drawing angle for bodies

--- physics_test/bodies.js
+++ physics_test/bodies.js
@@ -9,19 +9,38 @@ class Boxey {
     }
 
     show() {
+        rectMode(CENTER)
         var pos = this.body.position;
         var angle = this.body.angle;
 
         push();
         translate(pos.x, pos.y);
+        rotate(this.body.angle)
         rect(0, 0, this.w, this.h)
         pop();
     }
 }
 
-class Wall extends Boxey {
-    constructor() {
-        this.body
+class Obstacle {
+    constructor(x, y, w, h, world) {
+        const options = {
+            'isStatic': true
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.w = w;
+        this.h = h;
+        World.add(world, this.body);
+        this.world = world;
     }
 
+    show() {
+        rectMode(CENTER)
+        var pos = this.body.position;
+
+        push();
+        rotate(this.body.angle)
+        translate(pos.x, pos.y);
+        rect(0, 0, this.w, this.h);
+        pop();
+    }
 }
--- physics_test/phys_test.js
+++ physics_test/phys_test.js
@@ -6,6 +6,7 @@ var Engine = Matter.Engine,
 var engine;
 var world;
 var boxes = [];
+var obstacles = [];
 
 const RECT_WIDTH = 80;
 const RECT_HEIGHT = 80;
@@ -14,14 +15,23 @@ function setup() {
     createCanvas(400, 400);
     engine = Engine.create();
     world = engine.world;
+    createObstacles();
     boxes.push(new Boxey(width/2, height/2, RECT_WIDTH, RECT_HEIGHT, world));
     Engine.run(engine);
+}
+
+function createObstacles() {
+    obstacles.push(new Obstacle(width/2, height, width, 40, world))
 }
 
 function draw() {
     background(0);
 
     for (let i of boxes) {
+        i.show();
+    }
+
+    for (let i of obstacles) {
         i.show();
     }
 }
